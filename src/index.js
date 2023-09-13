79dbe0b4bd6b0443708c80d83a2f24f072d1ad17
Anthony Bosek <anthony.bosek@gmail.com>
2023-09-12 20:00:59 -0700

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const task = document.createElement("li");
+    task.innerText = e.target[0].value;
+    taskList.appendChild(task);
+    e.target.reset();
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "X";
+    task.appendChild(deleteButton);
+    deleteButton.addEventListener("click", function (e) {
+      e.target.parentNode.remove();
+    });
+  });
 });
